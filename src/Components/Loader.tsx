5e052e439ab356db132e5190c93ce6d3dfd926a7
JeuriMorel <rikonikage@yahoo.com>
2023-04-04 14:17:09 -0400

    skeleton style tweaks

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -4,10 +4,11 @@ function Loader() {
     return (
         <article className="flow flow-flexible">
             <Skeleton classes="skeleton-img" />
-            <div className="text-container flow">
+            <div className="text-container flow skeleton-text-container">
                 <Skeleton classes="skeleton-title" />
                 <Skeleton classes="skeleton-title" />
                 <Skeleton classes="skeleton-copyright" />
+                <Skeleton classes="skeleton-icon" />
             </div>
             <div className="skeleton-explanation-container flow explanation">
                 <Skeleton classes="skeleton-explanation" />
